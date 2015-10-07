7e0222f6bef2ea5e3fa7403e52999240f6cb3aab
canussie <canustralian@gmail.com>
2015-10-07 14:11:20 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# install the puppet yum repo rpm
+#
+rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
+#
+# install the puppet package using yum package manager
+#
+yum install puppet -y
+#
+# install nginx puppet module using the puppet agent
+#
+/opt/puppetlabs/bin/puppet module install jfryman/nginx
+#
+# install wget module for grabbing files from github
+#
+/opt/puppetlabs/bin/puppet module install maestrodev-wget
